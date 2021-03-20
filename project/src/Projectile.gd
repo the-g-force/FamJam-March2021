950c771d2ed702d11976930096e22452b0c38fa6
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-03-20 09:13:54 -0400

    Make turret turn and fire while there is a target
    The projectiles do not currently do anything.

---
+++ project/src/Projectile.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+export var speed := 100
+
+func _physics_process(delta):
+	var _ignored := move_and_collide(Vector2.RIGHT.rotated(rotation) * speed * delta)
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- project/src/Turret.gd
+++ project/src/Turret.gd
@@ -1,7 +1,45 @@
 extends Node2D
 
-# The node to track toward
-var target : Node2D setget _set_target
+const _Projectile := preload("res://src/Projectile.tscn")
 
-func _set_target(value:Node2D)->void:
-	print("My target is now %s" % value)
+# In radians per second
+export var rotation_speed := 0.1
+
+# The node to track toward
+var target : Node2D
+
+
+func _physics_process(_delta):
+	if target == null:
+		return
+	
+	var target_rotation := _compute_desired_rotation()
+	var full_amount := _short_angle_dist(rotation, target_rotation)
+	if full_amount < 0:
+		rotate(max(full_amount, -rotation_speed))
+	else:
+		rotate(min(full_amount, rotation_speed))
+	
+	
+
+func _compute_desired_rotation()->float:
+	var my_global_pos := get_global_transform().origin
+	var target_global_pos := target.get_global_transform().origin
+	var angle_to_target = (target_global_pos- my_global_pos).angle()
+	return angle_to_target
+
+
+# See https://gist.github.com/shaunlebron/8832585
+func _short_angle_dist(from, to)->float:
+	var max_angle = PI * 2
+	var difference = fmod(to - from, max_angle)
+	return fmod(2 * difference, max_angle) - difference
+
+
+func _on_ShotTimer_timeout():
+	if target != null:
+		var projectile := _Projectile.instance()
+		projectile.position = position
+		projectile.rotation = rotation
+		get_parent().add_child(projectile)
+		
