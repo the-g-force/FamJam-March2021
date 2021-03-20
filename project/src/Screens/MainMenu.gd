6ec33e6c3e55a137f801093d418e7f327f2e6285
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-03-20 14:50:45 -0400

    Add fullscreen toggle

--- project/src/Screens/MainMenu.gd
+++ project/src/Screens/MainMenu.gd
@@ -2,5 +2,15 @@ extends Control
 
 export var game_scene : PackedScene
 
+onready var _fullscreen_toggle := $FullscreenToggle
+
+func _ready():
+	_fullscreen_toggle.pressed = OS.window_fullscreen
+
+
 func _on_PlayButton_pressed():
 	var _ignored := get_tree().change_scene_to(game_scene)
+
+
+func _on_FullscreenToggle_pressed():
+	OS.window_fullscreen = _fullscreen_toggle.pressed
